fd940340aba862175c17a2b04d52ac36ba5547e9
Silas Sewell <silas@sewell.org>
2012-10-19 13:42:13 +0000

    Add gemrc

--- modules/silas/manifests/misc.pp
+++ modules/silas/manifests/misc.pp
@@ -5,4 +5,10 @@ class silas::misc {
     seltype => 'user_home_t',
     source  => 'puppet:///modules/silas/gdbinit',
   }
+  file { "${home}/.gemrc":
+    ensure  => present,
+    mode    => '0644',
+    seltype => 'user_home_t',
+    source  => 'puppet:///modules/silas/gemrc',
+  }
 }
